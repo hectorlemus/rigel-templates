0df4d76b8fe69f4ceb0d581b3d76df38e91a93a6
Héctor Lemus <dev.hectorlemus@gmail.com>
2021-01-15 19:00:14 -0600

    Demo

---
+++ src/public/utils.js
@@ -0,0 +1,12 @@
+'use strict';
+
+window.chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+    blue: 'rgba(35, 122, 187, 1)',
+    darkBlue: 'rgba(0, 32, 96, 1)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgba(165, 165, 165, 1)'
+};
