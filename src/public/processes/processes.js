0191137200bbd72fead1e1dc5a824759203dc159
Héctor Lemus <dev.hectorlemus@gmail.com>
2021-01-22 12:10:47 -0600

    Se egraga modal para retraso y planificacion.

--- src/public/processes/processes.js
+++ src/public/processes/processes.js
@@ -1,40 +1,40 @@
 
 var config = {
-    type: 'doughnut',
-    data: {
-        datasets: [{
-            data: [
-                4,
-                4,
-                2,
-            ],
-            backgroundColor: [
-                window.chartColors.darkBlue,
-                window.chartColors.blue,
-                window.chartColors.grey,
-            ],
-            label: 'Dataset 1'
-        }],
-        labels: [
-            'Espera',
-            'Ejecucion',
-            'Error',
-        ],
-    },
-    options: {
-        responsive: true,
-        legend: {
-            display: true,
-            position: 'right',
-        },
-        animation: {
-            animateScale: true,
-            animateRotate: true
-        }
-    }
+	type: 'doughnut',
+	data: {
+		datasets: [{
+			data: [
+				4,
+				4,
+				2,
+			],
+			backgroundColor: [
+				window.chartColors.darkBlue,
+				window.chartColors.blue,
+				window.chartColors.grey,
+			],
+			label: 'Dataset 1'
+		}],
+		labels: [
+			'Espera',
+			'Ejecucion',
+			'Error',
+		],
+	},
+	options: {
+		responsive: true,
+		legend: {
+			display: true,
+			position: 'right',
+		},
+		animation: {
+			animateScale: true,
+			animateRotate: true
+		}
+	}
 };
 
 window.onload = function() {
-    var ctx = document.getElementById('processChart').getContext('2d');
-    window.myDoughnut = new Chart(ctx, config);
+	var ctx = document.getElementById('processChart').getContext('2d');
+	window.myDoughnut = new Chart(ctx, config);
 };
